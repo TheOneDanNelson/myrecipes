46ca70effbf40db239bb5116b0f74bf635feae51
TheOneDanNelson <dannelson.manager@gmail.com>
2017-05-09 14:55:54 +0000

    added all of our CRUD functions and TDD

---
+++ test/integration/recipes_delete_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class RecipesDeleteTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chefname: "Dan", email: "dan@chef.com")
+    @recipe = Recipe.create(name: "Veggie Soup", description: "Great soup from the heartland", chef: @chef)
+  end 
+
+  test "successfully delete recipe" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+    assert_difference 'Recipe.count', -1 do
+      delete recipe_path(@recipe)
+    end
+    assert_redirected_to "recipe_path"
+    assert_not flash.empty?
+  end
+
+end
